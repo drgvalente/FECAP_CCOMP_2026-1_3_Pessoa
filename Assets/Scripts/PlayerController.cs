01084ed069c9dbd2ddaed276a7c0dacde41847e4
Adriano FV <54666006+drgvalente@users.noreply.github.com>
2026-04-13 09:11:49 -0300

    Intervalo 13/04/26

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,17 +11,21 @@ public class PlayerController : MonoBehaviour
 
     [Header("Referências")]
     public Transform cameraContainer;
+    public Transform gunContainer;
 
     [Header("Tiro")]
     public GameObject bulletPrefab;
     public Transform muzzle;
 
     private float verticalRotation = 0f;
+    private Rigidbody rb; // referência ao Rigidbody
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; // "some" com o cursor do mouse
+        rb = GetComponent<Rigidbody>();     // obtém o Rigidbody
+        rb.freezeRotation = true;           // impede o Rigidbody de rotacionar sozinho
     }
 
     // Update is called once per frame
@@ -31,17 +35,22 @@ public class PlayerController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         transform.Rotate(0f, mouseX, 0f);
 
-        // --- Rotação vertical da Camera (eixo X local) ---
+        // --- Rotação vertical da Camera e Arma (eixo X local) ---
         float mouseY = Input.GetAxis("Mouse Y");
         verticalRotation -= mouseY;
         verticalRotation = Mathf.Clamp(verticalRotation, -verticalClamp, verticalClamp);
         cameraContainer.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        gunContainer.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
 
         // --- Movimento WASD / Setas ---
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 direction = transform.right * h + transform.forward * v;
-        transform.position += direction * moveSpeed * Time.deltaTime;
+        //transform.position += direction * moveSpeed * Time.deltaTime;
+        // usa velocidade do Rigidbody ao invés de transform.position
+        // O eixo Y é preservado para que a gravidade continue funcionando normalmente
+        rb.linearVelocity = new Vector3(direction.x * moveSpeed, rb.linearVelocity.y, direction.z * moveSpeed);
+
 
         // --- Tiro ---
         if (Input.GetButtonDown("Fire1"))
